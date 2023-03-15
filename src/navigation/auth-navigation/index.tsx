c8d3a7456920ca9b09e2a0491df48ee64dc4ca66
Mohamed Youssef <mohamed.ahm9833@gmail.com>
2023-03-15 14:36:32 +0700

    Added Screens (Phone & Welcome ) to AuthStack

--- src/navigation/auth-navigation/index.tsx
+++ src/navigation/auth-navigation/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
 import {AuthParams, AuthParamList} from './type';
 import {RegisterScreen, LoginScreen} from '../../test';
+import {PhoneAuthScreen, WelcomeScreen} from '@Screens/index';
 
 const Stack = createNativeStackNavigator<AuthParamList>();
 
@@ -12,6 +12,11 @@ export const AuthNavigator = () => {
       screenOptions={{
         headerShown: false,
       }}>
+      <Stack.Screen name={AuthParams.Welcome} component={WelcomeScreen} />
+      <Stack.Screen
+        name={AuthParams.PhoneAuthentication}
+        component={PhoneAuthScreen}
+      />
       <Stack.Screen name={AuthParams.Login} component={LoginScreen} />
       <Stack.Screen name={AuthParams.Register} component={RegisterScreen} />
     </Stack.Navigator>
--- src/navigation/auth-navigation/type.ts
+++ src/navigation/auth-navigation/type.ts
@@ -3,11 +3,15 @@ import {StackNavigationProp} from '@react-navigation/stack';
 
 export enum AuthParams {
   Login = 'Login',
+  Welcome = 'Welcome',
   Register = 'Register',
+  PhoneAuthentication = 'PhoneAuthentication',
 }
 export type AuthParamList = {
   [AuthParams.Login]: NO_PARAMS;
   [AuthParams.Register]: NO_PARAMS;
+  [AuthParams.Welcome]: NO_PARAMS;
+  [AuthParams.PhoneAuthentication]: NO_PARAMS;
 };
 
 export type AuthNavigationType = StackNavigationProp<AuthParamList>;
