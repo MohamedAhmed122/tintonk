1868b19db46dde623b27edd5a43a4c9b51a372c8
Mohamed Youssef <mohamed.ahm9833@gmail.com>
2023-03-15 14:53:47 +0700

    fixed issue in `WithMobxProvider`

--- src/provider/with-mobx-provider.tsx
+++ src/provider/with-mobx-provider.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import {Provider as PaperProvider} from 'react-native-paper';
-import {theme} from '@Shared/theme';
 import stores from '@Shared/store';
 
 export const StoresContext = React.createContext(stores);
 
 export const WithMobxProvider = (component: () => React.ReactNode) => () =>
-  <PaperProvider theme={theme}>{component()}</PaperProvider>;
+  <StoresContext.Provider value={stores}>{component()}</StoresContext.Provider>;
